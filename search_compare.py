c4ab1d742758195c7e409aeb5032fe2f8041789b
“meandorca” <av.begreat@gmail.com>
2026-02-28 18:32:09 -0500

    added code for search and sort comparison

---
+++ search_compare.py
@@ -0,0 +1,6 @@
+def main():
+    print("Search comparison program is ready!")
+
+if __name__ == "__main__":
+    main()
+
---
+++ sort_compare.py
@@ -0,0 +1,5 @@
+def main():
+    print("Sort comparison program is ready!")
+
+if __name__ == "__main__":
+    main()
